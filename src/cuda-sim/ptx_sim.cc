1e537e9dd5543aeac6b402e443fc11050ba1da24
Ahmad <aalawneh@purdue.edu>
2025-07-12 04:21:56 +0000

    clock64 temp fix

--- src/cuda-sim/ptx_sim.cc
+++ src/cuda-sim/ptx_sim.cc
@@ -202,7 +202,7 @@ unsigned ptx_thread_info::get_builtin(int builtin_id, unsigned dim_mod) {
     case CLOCK_REG:
       return (unsigned)(m_gpu->gpu_sim_cycle + m_gpu->gpu_tot_sim_cycle);
     case CLOCK64_REG:
-      abort();  // change return value to unsigned long long?
+      // abort();  // change return value to unsigned long long?
                 // GPGPUSim clock is 4 times slower - multiply by 4
       return (m_gpu->gpu_sim_cycle + m_gpu->gpu_tot_sim_cycle) * 4;
     case HALFCLOCK_ID:
